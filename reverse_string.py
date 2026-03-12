a7c3b5cb8bbaea3a7dda3457c81850d0459952f4
sujanapaluru2008-123 <sujanapaluru2008@gmail.com>
2026-03-12 06:50:13 -0700

    Create reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,10 @@
+# Program to reverse a string
+text = input("Enter a string: ")
+
+# Slicing method to reverse
+reversed_text = text[::-1]
+print("Reversed string:", reversed_text)
+
+# Example output
+# Enter a string: github
+# Reversed string: buhtig
