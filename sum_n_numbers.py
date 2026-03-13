1ac4bef3fbb7001c44198405414f6dd6d90686e0
sujanapaluru2008-123 <sujanapaluru2008@gmail.com>
2026-03-12 17:04:43 -0700

    Create sum_n_numbers.py

---
+++ sum_n_numbers.py
@@ -0,0 +1,12 @@
+# Program to find sum of first n natural numbers
+n = int(input("Enter a number: "))
+total = 0
+
+for i in range(1, n+1):
+     total += 1
+
+print("Sum: ", total)
+
+# Output
+# Enter a number: 5
+# Sum: 15
